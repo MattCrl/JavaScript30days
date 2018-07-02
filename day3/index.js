c504ff7e5e994d5c2621a49b484510645a53a1da
Matt Crl <matth.carlevaris@gmail.com>
2018-07-02 15:19:22 +0200

    day 3 almost done, need to fix black & white filter

--- day3/index.js
+++ day3/index.js
@@ -1,1 +1,19 @@
-const inputs = document.querySelectorAll('.controls.input');
+const inputs = document.querySelectorAll('.controls input');
+const choose = document.querySelector('.choose input');
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+function blackwhite() {
+    console.log(choose.checked);
+    choose.checked ? document.documentElement.style.setProperty('filter', 'grayscale(100%)') :
+                     document.documentElement.style.setProperty('filter', '');
+
+}
+
+choose.addEventListener('change', blackwhite);
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
